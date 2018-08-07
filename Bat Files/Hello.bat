cfa8c7432f6f4661d33c369d424c80f1c0577450
Dheeraj Sing <Dheerajps77@gmail.com>
2018-08-07 20:24:26 +0530

    This is demo for jenkins

---
+++ Bat Files/Hello.bat
@@ -0,0 +1,4 @@
+d:
+cd D:\Eclipse selenium Program\JenkinsDemo
+set classpath=D:\Eclipse selenium Program\JenkinsDemo\bin;D:\Eclipse selenium Program\JenkinsDemo\Library\*;
+java org.testng.TestNG testng.xml
